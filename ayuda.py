6fcc89d14aee1c35dbe844aac5144f9ad1dad3d8
Josward <josjapu111@gmail.com>
2022-12-03 10:32:09 -0400

    Codigo funcional

---
+++ ayuda.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 10:15:08 2022
+
+@author: Josward
+"""
+a=0
+
+while a==3:
+    
+ print("Ganaste")
+ a=a+1
+
+
+  
+    
---
+++ print.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 10:15:06 2022
+
+@author: Josward
+"""
+
+print("gkggdlgpd")
