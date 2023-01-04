13ef21c81fcc6b50a42cd9197bc6abcd05b15ca3
Josward <josjapu111@gmail.com>
2023-01-03 21:45:32 -0400

    2/7 Pandas y Python

--- Gestion de Autos.py
+++ Gestion de Autos.py
@@ -1,6 +1,7 @@
 
-import csv 
+import pandas as pd
 
+import matplotlib.pyplot as plt
 cont=0;
 
 
@@ -24,29 +25,67 @@ def hora(a):
    print(a)  
    
    
-   
-with open("autos.csv") as arch:
-   
-   leer = csv.reader(arch)
-   for linea in leer:
-       cont+=1
-       print( linea[0])
+autos=pd.read_csv("autos.csv",index_col="index")
 
 
-   
+#1 Mayor modelo de carro con mas ingreso
+#Masingreso=autos.groupby("model").agg({"price":sum})
+#mayor=Masingreso["price"].max()
+#print(Masingreso[Masingreso["price"]==mayor])
 
+#2 Ventas por tipos de autos (Automatico o Manual)
+a=list(autos["gearbox"])
 
-
-
-
-
-
+def Caja(a):
+ """Toma una lista y verfica cual de los tipos de autos fue vas vendidos"""
+ b=0
+ c=0 
+ for i in a:
  
+  if i=="manuell":
+    b=b+1
+  else:
+    c=c+1
 
-
-
-    
+    if b>c:
+        return "El manual tuvo mas ventas"
+    else:
+        return "El automatico tuvo mas ventas"
    
+     
+  
+   
+numero=Caja(a)
+print(numero)
 
 
+
+
+
+
+
+
+
+#Totalautos=autos.groupby("name").mean()
+
+#Totalautos=autos.groupby("name").agg({"price":sum})
+    
+#print(Totalautos)
+
+#autos.plot(kind="scatter",x="powerPS", y="kilometer")
+#plt.show()
+
+#print(autos[(autos["price"]>40000) & (autos["index"]>300000)].head())
+
+#print(autos.loc[[5,7],"price"])  
+
+#print(autos.loc[[5,7]])  
+
+#print(autos.iloc[1:4])
+
+#print(autos.describe())   
+
+#precios=autos["price"]>1000
+
+#print(precios["price"].head())
 
